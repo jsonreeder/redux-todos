ce71bf165b0e0d1ccec17c74bbe3b85661af2d3b
Jason Reeder <info@jasonreeder.com>
2017-03-06 17:19:41 -0800

    Allow access to todo actions on window
    For testing

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
 
+// TODO: Remove after development
+import { receiveTodo, receiveTodos } from "./actions/todo_actions";
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('content');
   const store = configureStore();
@@ -10,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // TODO: Remove after development
   window.store = store;
   window.receiveTodo = receiveTodo;
+  window.receiveTodos = receiveTodos;
 
   ReactDOM.render(<Root store={store} />, root);
 });
